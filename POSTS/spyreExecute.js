a91380f6203064365d03d566651ee1264b299907
David Wilson <david.wilson@govsacademy.org>
2016-02-24 11:37:20 -0500

    update

--- POSTS/spyreExecute.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	enter: function(params) {
-		var fs = require("fs");
-		if (params.source) {
-			fs.writeFileSync("./tmp/spyresrc.spys", params.source);
-			require("child_process").exec("./spyre ./tmp/spyresrc.spys");
-		}
-	}
-};
